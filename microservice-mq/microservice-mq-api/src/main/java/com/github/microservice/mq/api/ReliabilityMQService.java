398b44c55cae001e9bfa21644efe30641c3a06a0
zhaoxiufei@trgroup.cn <123321qwe>
2019-06-12 15:48:17 +0800

    init

---
+++ microservice-mq/microservice-mq-api/src/main/java/com/github/microservice/mq/api/ReliabilityMQService.java
@@ -0,0 +1,11 @@
+package com.github.microservice.mq.api;
+
+/**
+ * 可靠消息服务
+ * @author <a href="mailto:zhaoxiufei@gmail.com">赵秀非</a>
+ * @since 2019-06-10 上午10:55
+ */
+public interface ReliabilityMQService {
+
+
+}
