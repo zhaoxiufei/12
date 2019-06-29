8a7cb49aba3602b2c7fbcee7ce7cee0e4b170a87
zhaoxiufei <zhaoxiufei@trgroup.cn>
2019-06-29 11:00:45 +0800

    init

--- microservice-shardingjdbc/microservice-shardingjdbc-masterslave/src/main/java/com/github/microservice/shardingjdbc/masterslave/controller/TestController.java
+++ microservice-shardingjdbc/microservice-shardingjdbc-masterslave/src/main/java/com/github/microservice/shardingjdbc/masterslave/controller/TestController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author <a href="mailto:zhaoxiufei@gmail.com">赵秀非</a>
  * @since 2019-06-28 13:56
  */
+@SuppressWarnings("SpringJavaAutowiredFieldsWarningInspection")
 @RestController
 @RequestMapping
 public class TestController {
@@ -19,6 +20,9 @@ public class TestController {
     @GetMapping("t1")
     public String t1() {
         userService.save();
+        userService.find();
+        userService.find();
+        userService.find();
         userService.find();
         userService.findMaster();
         return "ok";
